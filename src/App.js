6a2cbda895e62915c0114d8f10a207edd2014529
DanielPortelaByrne <danielportelabyrne@gmail.com>
2023-03-05 16:59:43 +0000

    Changes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo512.png";
+import google from "./google-play-badge.png";
+import apple from "./app-store-badge.png";
+import "./App.css";
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} alt="logo" className="App-logo" />
+        <h3>Get the App</h3>
+        <div className="badges">
+          <a href="https://play.google.com/store/apps/details?id=com.danielportelabyrne.drinkingdemocracy">
+            <img src={google} alt="Get it on Google Play" />
+          </a>
+          <a href="https://apps.apple.com/us/app/your-app-name/id1234567890">
+            <img src={apple} alt="Get it on the App Store" />
+          </a>
+        </div>
+        <h2>Contact Information</h2>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Email:{" "}
+          <a href="mailto:drinkingdemocracy@gmail.com">
+            drinkingdemocracy@gmail.com
+          </a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <p>Phone: (123) 456-7890</p> */}
+        <h3>Privacy Policy</h3>
+        <p>
+          We take your privacy seriously and will not share your information
+          with any third parties.
+        </p>
       </header>
     </div>
   );
